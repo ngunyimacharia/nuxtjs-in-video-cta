7922b63bf8a8adb5bbd9686f2f97d5afe8ac25a3
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-12-14 17:49:01 +0200

    Description of tutorial added

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="w-2/3 h-96 my-10 mx-auto relative">
-    <video
-      id="video-player"
-      controls
-      autoplay
-      muted
-      class="cld-video-player cld-video-player-skin-dark w-full h-full"
-    >
-    </video>
-    <a 
-      v-if="percentage > 20 && percentage < 70"
-      href="https://www.pexels.com/video/aerial-view-on-city-during-dawn-9722139/" 
-      target="_blank"
-      class="inline-flex items-center px-4 py-2 opacity-50 hover:opacity-70 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-white bg-transparent hover:text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 absolute top-10 right-10"
-    >
-      View on Pexels
-    </a>
-    <img 
-      v-if="paused || ended"
-      src="https://res.cloudinary.com/hackit-africa/image/upload/v1639495990/nuxtjs-in-video-cta/free-video-856479.jpg" 
-      class="absolute w-96 top-16 left-72"
-    />
+  <div>
+    <h1 class="my-10 block text-center leading-8 font-extrabold tracking-tight text-gray-600 text-2xl"> How to show CTAs within and after videos in Nuxt.Js</h1>
+
+    <div class="w-2/3 h-96 my-10 mx-auto relative">
+
+      <video
+        id="video-player"
+        controls
+        autoplay
+        muted
+        class="cld-video-player cld-video-player-skin-dark w-full h-full"
+      >
+      </video>
+      <a 
+        v-if="percentage > 20 && percentage < 70"
+        href="https://www.pexels.com/video/aerial-view-on-city-during-dawn-9722139/" 
+        target="_blank"
+        class="inline-flex items-center px-4 py-2 opacity-50 hover:opacity-70 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-white bg-transparent hover:text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 absolute top-10 right-10"
+      >
+        View on Pexels
+      </a>
+      <img 
+        v-if="paused || ended"
+        src="https://res.cloudinary.com/hackit-africa/image/upload/v1639495990/nuxtjs-in-video-cta/free-video-856479.jpg" 
+        class="absolute w-96 top-16 left-72"
+      />
+    </div>
+
+    <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto leading-10 w-2/3 mx-auto">
+      The above video will show two call to actions:
+      <br/>
+      <b>View on Pexels:</b> When the percentage played is greater than 20% but less than 70%
+      <br/>
+      <b>Free Stock Videos:</b> When the video is either paused or ends
+    </div>
   </div>
 </template>
 
